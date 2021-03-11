62c6dba3e09a13f9303fa216f946163f581ec72b
akankshast <akankshatamboli195@gmail.com>
2021-03-11 23:27:28 +0530

    forms added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,20 +3,6 @@ from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        "author": "akanksha",
-        "title": "post 1",
-        "content": "first blog post",
-        "date": "7th march",
-    },
-    {
-        "author": "jyoti",
-        "title": "post 2",
-        "content": "second blog post",
-        "date": "7th march",
-    },
-]
 
 
 def home(request):
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "crispy_forms",
+    "users.apps.UsersConfig",
     "blog.apps.BlogConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
