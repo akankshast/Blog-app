290fcafe6fbfd57b06115b1a5e108ac6c78850cf
akankshast <akankshatamboli195@gmail.com>
2021-03-12 23:42:27 +0530

    login functionality added

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = "/static/"
 
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "blog-home"
+LOGIN_URL = "login"
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -9,8 +10,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username} ")
-            return redirect("blog-home")
+            messages.success(request, f"Your account has been created! ")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
