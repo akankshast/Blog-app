c478e52e9f9c90d5970f33414a2b27ba451e2a1f
akankshast <akankshatamboli195@gmail.com>
2021-03-16 14:28:40 +0530

    update from added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 def register(request):
@@ -19,4 +19,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+    context = {"u_form": u_form, "p_form": p_form}
+
+    return render(request, "users/profile.html", context)
