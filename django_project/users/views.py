5310f0b03e5189d3b0cbc227fecdb57cda77c3a3
akankshast <akankshatamboli195@gmail.com>
2021-03-16 16:20:50 +0530

    image upload fixed

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -19,8 +19,19 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Profile Updated Successfully")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {"u_form": u_form, "p_form": p_form}
 
     return render(request, "users/profile.html", context)
