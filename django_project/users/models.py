3062a7fa170f56b464f0866e62519b8dbd4a108c
akankshast <akankshatamboli195@gmail.com>
2021-03-28 01:47:50 +0530

    error resolved

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
         return f"{self.user.username}Profile"
 
     def save(self):
-        super.save()
+        super().save()
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
